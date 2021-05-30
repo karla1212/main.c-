dcb17c7c402e6fb644ffdea6c33da7d2bcfa9852
karla1212 <85045762+karla1212@users.noreply.github.com>
2021-05-30 12:04:52 -0500

    pude hacerlo
    con mucha dedicacion

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
-}
+int  main ()
+	{
+	  int i;
+	  para (i = 0 ; i < 32 ; i ++)
+	  {
+	    printf ( " Hola Escuela \ n " );
+	  }
+	  return  0 ;
+	}
